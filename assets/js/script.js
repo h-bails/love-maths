53e975218064747e40474b05a9c32425348f5f42
h-bails <hollyannebailey@gmail.com>
2023-01-04 21:28:27 +0000

    Tweak division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -106,6 +106,10 @@ function incrementWrongAnswer() {
     document.getElementById("incorrect").innerText = ++oldScore;
 }
 
+/**
+ * Displays the questions according to the
+ */
+
 function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
@@ -125,7 +129,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivideQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand1').textContent = (operand1 * operand2);
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "/";
 } 
